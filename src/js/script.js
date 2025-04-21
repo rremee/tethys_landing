ae176e871981f4ea9cebcfc6d93e050e58218be6
Anton Valkovskyi <u1remited@gmail.com>
2025-04-21 08:50:24 +0300

    header + burger menu (+ script for it); small changes $fontSize (vars.scss); button components created (button.scss)

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 import "/src/sass/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const burger = document.querySelector(".burger");
+	const menu = document.querySelector(".menu");
+	const body = document.body;
+
+	burger.addEventListener("click", () => {
+		const isActive = menu.classList.toggle("menu--active");
+		burger.classList.toggle("burger-close", isActive);
+		body.style.overflow = isActive ? "hidden" : "";
+	});
+});
