aef36e2f8bdcb32e274ae0322cb430880bc7830f
Anton Valkovskyi <u1remited@gmail.com>
2025-04-25 10:46:19 +0300

    observer script (header, hero and feature section animated); scripts for correct animation for decor images; new animations for observer (animation.scss); universal mixin for observe animations;

--- src/js/script.js
+++ src/js/script.js
@@ -11,8 +11,59 @@ document.addEventListener("DOMContentLoaded", () => {
 		body.style.overflow = isActive ? "hidden" : "";
 	});
 
-	const circles = document.querySelectorAll(".circles__item");
-	circles.forEach((circle, i) => {
-		circle.style.animationDelay = `${i * 100}ms`;
+	const circleContainers = document.querySelectorAll(".circles");
+
+	circleContainers.forEach((circleContainer) => {
+		const circles = circleContainer.querySelectorAll(".circles__item");
+		circles.forEach((circle, i) => {
+			circle.style.animationDelay = `${i * 140}ms`;
+		});
+	});
+
+	const decorImages = document.querySelectorAll(".decor-image");
+
+	decorImages.forEach((el) => {
+		el.addEventListener("animationend", (e) => {
+			if (e.animationName === "rotateIn") {
+				const transformOrig = getComputedStyle(el).transform;
+
+				const keyframes = [
+					{ transform: `${transformOrig} translateY(0)` },
+					{ transform: `${transformOrig} translateY(-1rem)` },
+					{ transform: `${transformOrig} translateY(0)` },
+				];
+
+				el.animate(keyframes, {
+					duration: 3000,
+					iterations: Infinity,
+					easing: "ease-in-out",
+				});
+			}
+		});
+	});
+
+	const options = {
+		root: null,
+		rootMargin: "0px 0px 0px 0px",
+		threshold: 0.2,
+	};
+
+	const callback = (entries, observer) => {
+		entries.forEach((entry) => {
+			const currentElement = entry.target;
+			if (entry.isIntersecting) {
+				currentElement.classList.add("--animate");
+			} else {
+				currentElement.classList.remove("--animate");
+			}
+		});
+	};
+
+	const observer = new IntersectionObserver(callback, options);
+
+	const animElements = document.querySelectorAll('[class*="--anim"]');
+
+	animElements.forEach((animElement) => {
+		observer.observe(animElement);
 	});
 });
